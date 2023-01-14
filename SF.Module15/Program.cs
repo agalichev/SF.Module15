3c75bf5dd8993566ffd31ad038d6b1ebf747b9a0
Alexander Galichev <lex.galichev@gmail.com>
2023-01-15 10:22:35 +1200

    SF.Module15 - Added comleted SF.Module15.csproj, Classroom.cs. Edited README.md.

--- SF.Module15/Program.cs
+++ SF.Module15/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var classes = new[]
+            {
+                new Classroom { Student = { "Evgeniy", "Sergey", "Andrew" }, },
+                new Classroom { Student = { "Anna", "Viktor", "Vladimir" }, },
+                new Classroom { Student = { "Bulat", "Alex", "Galina" } }   
+            };
+
+            // Получаем массив имен всех студентов
+            var allStudents = GetAllStudents(classes);
+
+            Console.WriteLine("Вот список всех студентов:");
+            Console.WriteLine(string.Join(" ", allStudents));
+
+            Console.ReadLine();
+        }
+
+        // Метод, который делает выборку с нескольких коллекций и возвращает все в одном массиве строк
+        static string[] GetAllStudents(Classroom[] classes)
+        {
+            return (from classroom in classes
+                    from student in classroom.Student
+                    select student).ToArray();
         }
     }
 }
